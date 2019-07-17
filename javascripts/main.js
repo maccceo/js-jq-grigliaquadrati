5b4df130728f9152cb70661ac0e3a20457aa0277
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-17 15:59:22 +0200

    Esercizio base completato

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,20 @@
+// Creare una griglia formata da 8x8 quadratini tutti bianchi.
+// 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
+
+// aspetto che il documento sia stato caricato interamente
 $(document).ready(function() {
 
-}
+	//azione al click di un quadratino
+	$('.quadratino').click(function() {
+
+		//controllo se il quadratino va colorato di rosso
+		if ($(this).attr('rosso') === 'true') {
+			$(this).addClass("rosso");
+		}
+
+		//altrimenti coloro di verde
+		else {
+			$(this).addClass("verde");
+		}
+	});
+});
