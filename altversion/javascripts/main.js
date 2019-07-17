a937b2d87d90ad06fb517c3da37cac6ac30847db
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-17 17:33:19 +0200

    altv - il programma funziona correttamente

--- altversion/javascripts/main.js
+++ altversion/javascripts/main.js
@@ -4,7 +4,7 @@
 // 2 - generazione dinamica,  random,  della griglia (anche qui potrei scomporre in 2 problemi, prima mi preoccupo di come generala da js e poi in caso, come rendere random la distribuzione dei rettangoli rossi).
 var redCounter = 0, greenCounter = 0;
 var redSquares = squaresGenerator(15, 64);
-//argomenti passati: 1) n° numeri da generare, 2) valore massimo
+var row, remainder, position, found = false;
 console.log(redSquares);
 
 // aspetto che il documento sia stato caricato interamente
@@ -12,7 +12,31 @@ $(document).ready(function() {
 
 	//azione al click di un quadratino
 	$('.quadratino').click(function() {
+		//ricavo posizione numerica del quadrato (1-64)
+		row = parseInt( $(this).attr('riga') );
+		remainder = parseInt( $(this).attr('resto') );
+		position = row * 8 + remainder;
 
+		//controllo se è un quadratino rosso
+		for (var i = 0; i < redSquares.length; i++) {
+			if (position === redSquares[i]) {
+				found = true;
+			}
+		}
+
+		//coloro opportunamente il quadratino
+		if (found) {
+			$(this).addClass("rosso");
+			//visualizzo il punteggio
+			redCounter++;
+			$("#redScoreboard").text(redCounter);
+			found = false;
+		} else {
+			$(this).addClass("verde");
+			//visualizzo il punteggio
+			greenCounter++;
+			$("#greenScoreboard").text(greenCounter);
+		}
 	});
 });
 
