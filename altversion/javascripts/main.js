388c8fbb9e9dabeeb632f8e2cfcbcfc4982f4d75
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-17 16:59:29 +0200

    altv - generazione casuale posizione quadrati rossi

--- altversion/javascripts/main.js
+++ altversion/javascripts/main.js
@@ -3,9 +3,9 @@
 // 1 - Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti;
 // 2 - generazione dinamica,  random,  della griglia (anche qui potrei scomporre in 2 problemi, prima mi preoccupo di come generala da js e poi in caso, come rendere random la distribuzione dei rettangoli rossi).
 var redCounter = 0, greenCounter = 0;
-var redScoreboard = $("#redScoreboard");
-var greenScoreboard = $("#greenScoreboard");
-
+var redSquares = squaresGenerator(15, 64);
+//argomenti passati: 1) n° numeri da generare, 2) valore massimo
+console.log(redSquares);
 
 // aspetto che il documento sia stato caricato interamente
 $(document).ready(function() {
@@ -13,18 +13,29 @@ $(document).ready(function() {
 	//azione al click di un quadratino
 	$('.quadratino').click(function() {
 
-		//controllo se il quadratino va colorato di rosso
-		if ($(this).attr('rosso') === 'true') {
-			$(this).addClass("rosso");
-			redCounter++;
-			redScoreboard.text(redCounter);
-		}
-
-		//altrimenti coloro di verde
-		else {
-			$(this).addClass("verde");
-			greenCounter++;
-			greenScoreboard.text(greenCounter);
-		}
 	});
 });
+
+
+// # # FUNZIONI # #
+function squaresGenerator(howMany, maxValue) {
+	var numGenerated, array = [];
+
+	for (var i = 1; i < howMany; i++) {
+		//genero un numero
+		numGenerated = Math.floor(Math.random() * maxValue + 1);
+		//controllo che non sia già stato generato in precedenza
+		for (var i = 0; i < array.length; i++) {
+				if (array[i] === numGenerated) {
+					numGenerated = Math.floor(Math.random() * maxValue + 1);
+					// ricontrollo da capo per evitare che il numero nuovo sia uguale a uno precedente
+					i = -1;
+				}
+			}
+		//pusho il numero generato (sicuramente non doppio) nell'array
+		array.push(numGenerated)
+	}
+	//ordino per comodità, non necessario
+	array = array.sort();
+	return array;
+}
